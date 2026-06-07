a28b184e8340bd0e2b84dcbd5ef725ffa36d9095
가동재 <masterforce999@naver.com>
2026-06-07 19:46:53 +0900

    Update OG share image

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -8,7 +8,7 @@ export function isLocale(value: string): value is Locale {
 export const siteConfig = {
   name: "2027 WYD 부산교구대회 공식 사이트",
   url: "https://www.wyd2027did-busan.org",
-  ogImage: "/logo.svg",
+  ogImage: "https://r2-image-server.masterforce999.workers.dev/opengraph-image.png",
 };
 
 export const content = {
